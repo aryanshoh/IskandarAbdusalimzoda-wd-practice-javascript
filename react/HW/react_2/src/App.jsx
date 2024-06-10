a69d5724af749699c6259b814da426c0b770e7e0
Iskandar <75279834+aryanshoh@users.noreply.github.com>
2024-06-10 16:17:50 +0300

    Add files via upload

---
+++ react/HW/react_2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function NavigationBar() {
+    // TODO: Actually implement a navigation bar
+    return (
+        <div>
+            <h1>Hello from React!</h1>
+            <h1>Какое то сообщение</h1>
+        </div>
+    );
+}
+
+export default NavigationBar;
